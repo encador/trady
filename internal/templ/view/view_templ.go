4cecc67065b2a73f32c02e07110a1c09284bae6a
encador <encador@proton.me>
2026-04-10 20:39:02 -0400

    closes #4
    closes #4

--- internal/templ/view/view_templ.go
+++ internal/templ/view/view_templ.go
@@ -29,7 +29,7 @@ func Hello() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html><body><p>Hello, World!</p></body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html><body><h1>Hello, World!</h1></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
