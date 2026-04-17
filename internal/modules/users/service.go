f0144bbce400c7b31e3d16fe6e803e64eec1c77e
encador <encador@proton.me>
2026-04-17 09:52:55 -0400

    modify input validation

--- internal/modules/users/service.go
+++ internal/modules/users/service.go
@@ -3,6 +3,7 @@ package users
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 
 	"github.com/encador/trady/internal/models"
 )
@@ -11,11 +12,21 @@ func addUser(user models.User, db *sql.DB) []string {
 	errs := []string{}
 
 	// Validate User Input
-	if user.Username == "" || user.Username == "bob" {
-		errs = append(errs, "Name: Connot Be Empty")
+	if len(strings.Fields(user.Username)) > 1 {
+		errs = append(errs, "Name: Must be a Single Word")
 	}
+
+	username := strings.TrimSpace(user.Username)
+	if username == "" {
+		errs = append(errs, "Name: Connot be Empty")
+	} else if len(username) > 24 {
+		errs = append(errs, "Name: Connot Exceed 24 Characters")
+	}
+
 	if user.Password == "" {
-		errs = append(errs, "Password: Connot Be Empty")
+		errs = append(errs, "Password: Connot be Empty")
+	} else if len(user.Password) > 64 {
+		errs = append(errs, "Password: Connot Exceed 64 Characters")
 	}
 	if len(errs) != 0 {
 		return errs
@@ -23,7 +34,7 @@ func addUser(user models.User, db *sql.DB) []string {
 
 	// Add User to Database
 	q := `insert into users(username, password) values(?,?)`
-	if _, err := db.Exec(q, user.Username, user.Password); err == nil {
+	if _, err := db.Exec(q, username, user.Password); err == nil {
 		errs = append(errs, "Success")
 	} else {
 		fmt.Println(err)
