a0d962946a5e88736a0cdc2a390a42768aef45e2
encador <encador@proton.me>
2026-04-20 08:30:40 -0400

    add address and port to config
    configure the port and address on which applicaiton runs as command line
    arguments

--- main.go
+++ main.go
@@ -12,11 +12,6 @@ import (
 	"github.com/encador/trady/internal/templ/component"
 )
 
-type config struct {
-	dbPath string
-	init   bool
-}
-
 func logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// t := time.Now().Format("1-02 15:04:05")
@@ -26,9 +21,18 @@ func logger(next http.Handler) http.Handler {
 	})
 }
 
+type config struct {
+	address string
+	port    int
+	dbPath  string
+	init    bool
+}
+
 func main() {
 
 	var cnf config
+	flag.StringVar(&cnf.address, "address", "localhost", "address on which the application runs")
+	flag.IntVar(&cnf.port, "port", 55000, "Port # for the application")
 	flag.StringVar(&cnf.dbPath, "db-path", "trady.db", "sqlite3 database file")
 	flag.BoolVar(&cnf.init, "init", true, "initialize application files when missing")
 	flag.Parse()
@@ -61,8 +65,10 @@ func main() {
 	mux.Handle("/user", userH.HandleUserPage())
 	mux.Handle("/user/new", userH.HandleAdd())
 
-	fmt.Println("[LOG] Serving on localhost:555000")
-	err = http.ListenAndServe("localhost:55000", logger(mux))
+	adr := fmt.Sprintf("%s:%d", cnf.address, cnf.port)
+
+	fmt.Println("[LOG] Serving on " + adr)
+	err = http.ListenAndServe(adr, logger(mux))
 	fmt.Println(err)
 
 }
